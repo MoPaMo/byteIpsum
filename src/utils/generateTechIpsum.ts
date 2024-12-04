4234035c398e2652a48e8077350292332131c824
MoPaMo <mopamo@users.noreply.github.com>
2024-12-04 23:14:29 +0100

    refactor: clean up formatting and improve readability in generateTechIpsum function

--- src/utils/generateTechIpsum.ts
+++ src/utils/generateTechIpsum.ts
@@ -1,25 +1,27 @@
+import { nouns, adjectives, interpositions } from "./wordlist";
 
-import {nouns, adjectives, interpositions} from './wordlist';
-
-
-export default function generateTechIpsum(paragraphs: number, wordsPerParagraph: number): string {
-const ipsum: string[] = [];
-for (let i = 0; i < paragraphs; i++) {
+export default function generateTechIpsum(
+  paragraphs: number,
+  wordsPerParagraph: number
+): string {
+  const ipsum: string[] = [];
+  for (let i = 0; i < paragraphs; i++) {
     const paragraph: string[] = [];
     for (let j = 0; j < wordsPerParagraph; j++) {
-        const noun = nouns[Math.floor(Math.random() * nouns.length)];
-        let adjective: string;
-        if(Math.random() < 0.1) {
-            adjective = adjectives[Math.floor(Math.random() * adjectives.length)] + ' and ' + adjectives[Math.floor(Math.random() * adjectives.length)];
-        }
-        else{
-            adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-        }
-        const interposition = interpositions[Math.floor(Math.random() * interpositions.length)];
-        paragraph.push(`${adjective} ${noun} ${interposition}`);
+      const noun = nouns[Math.floor(Math.random() * nouns.length)];
+      let adjective: string;
+      if (Math.random() < 0.1) {
+        adjective =
+          adjectives[Math.floor(Math.random() * adjectives.length)] +
+          " and " +
+          adjectives[Math.floor(Math.random() * adjectives.length)];
+      } else {
+        adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+      }
+      const interposition =
+        interpositions[Math.floor(Math.random() * interpositions.length)];
+      paragraph.push(`${adjective} ${noun} ${interposition}`);
     }
-    ipsum.push(paragraph.join(' '));
-
-
-}
+    ipsum.push(paragraph.join(" "));
+  }
 }
