b6ca3dc9cf99a9996cca6d59f3db1ab3d947d633
MoPaMo <mopamo@users.noreply.github.com>
2024-12-04 18:29:45 +0100

    add wordlist utility with predefined nouns

---
+++ src/utils/wordlist.tsx
@@ -0,0 +1,14 @@
+const nouns: string[] = [
+    "algorithm",
+    "application",
+    "architecture",
+    "archive",
+    "artificial intelligence",
+    "bandwidth",
+    "capability",
+    "capacity",
+
+]
+
+
+export default {nouns};
